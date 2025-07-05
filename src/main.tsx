5e93bdc821d08cab6ab9257eec4ee6d22a1baedd
dav4os <davi4os99@gmail.com>
2025-07-05 21:01:57 +0300

    Отмена изменений - возврат к исходному состоянию

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,6 @@ import { HelmetProvider } from 'react-helmet-async';
 import App from './App.tsx';
 import './index.css';
 import { reportWebVitals } from './utils/performance';
-import { validateEnv } from './config/env';
-import { initAnalytics } from './utils/analytics';
-
-// Валидируем переменные окружения
-validateEnv();
-
-// Инициализируем аналитику
-initAnalytics();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -26,8 +18,6 @@ createRoot(document.getElementById('root')!).render(
 
 // Measure performance
 reportWebVitals((metric) => {
-  if (process.env.NODE_ENV === 'development') {
-    console.log(metric);
-  }
+  console.log(metric);
   // Here you would send metrics to your analytics service
 });
