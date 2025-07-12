28079f76f24b27e2ff6d861ce3fbd9b5241a2e6e
dav4os <davi4os99@gmail.com>
2025-07-12 22:07:24 +0300

    справлен SEOHead: добавлен HelmetProvider в App.tsx для корректной работы react-helmet-async

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import ErrorBoundary from './components/ErrorBoundary';
 import HomePage from './components/HomePage';
 import BlogPage from './components/BlogPage';
@@ -11,21 +12,23 @@ import SEOAnalyzer from './components/SEOAnalyzer';
 
 function App() {
   return (
-    <ErrorBoundary>
-      <div className="min-h-screen">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/blog" element={<BlogPage />} />
-          <Route path="/blog/:slug" element={<BlogArticle />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/rental" element={<CarRentalPage />} />
-        </Routes>
-        
-        {/* Development tools */}
-        <PerformanceMonitor />
-        <SEOAnalyzer />
-      </div>
-    </ErrorBoundary>
+    <HelmetProvider>
+      <ErrorBoundary>
+        <div className="min-h-screen">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/blog" element={<BlogPage />} />
+            <Route path="/blog/:slug" element={<BlogArticle />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/rental" element={<CarRentalPage />} />
+          </Routes>
+          
+          {/* Development tools */}
+          <PerformanceMonitor />
+          <SEOAnalyzer />
+        </div>
+      </ErrorBoundary>
+    </HelmetProvider>
   );
 }
 
